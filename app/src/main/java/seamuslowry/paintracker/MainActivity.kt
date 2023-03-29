bd606eed694107ad5c55db14864ba384022e79c2
Seamus Lowry <spl1owry3@gmail.com>
2023-03-29 19:18:24 -0400

    feat/initial screen setup (#4)
    * add more icons library
    
    * add initial navigation

--- app/src/main/java/seamuslowry/paintracker/MainActivity.kt
+++ app/src/main/java/seamuslowry/paintracker/MainActivity.kt
@@ -6,11 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
+import seamuslowry.paintracker.ui.navigation.Navigation
+import seamuslowry.paintracker.ui.navigation.Screen
 import seamuslowry.paintracker.ui.theme.PainTrackerTheme
 
 @AndroidEntryPoint
@@ -24,22 +23,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    Greeting("Android")
+                    Navigation(startDestination = Screen.Configuration.identifier)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PainTrackerTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/seamuslowry/paintracker/ui/screens/report/ReportScreen.kt
@@ -0,0 +1,9 @@
+package seamuslowry.paintracker.ui.screens.report
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ReportScreen() {
+    Text(text = "report")
+}
