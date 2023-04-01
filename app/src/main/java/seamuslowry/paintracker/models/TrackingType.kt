6fb44d50e9c737637fb679acea1675ee48e3f3c9
Seamus Lowry <spl1owry3@gmail.com>
2023-04-01 19:11:33 -0400

    update input form

--- app/src/main/java/seamuslowry/paintracker/models/TrackingType.kt
+++ app/src/main/java/seamuslowry/paintracker/models/TrackingType.kt
@@ -4,3 +4,7 @@ enum class TrackingType {
     ONE_TO_TEN,
     YES_NO,
 }
+
+fun TrackingType.relative(offset: Int): TrackingType {
+    return TrackingType.values()[this.ordinal.plus(offset).mod(TrackingType.values().size)]
+}
--- app/src/main/java/seamuslowry/paintracker/ui/screens/configuration/ConfigurationScreen.kt
+++ app/src/main/java/seamuslowry/paintracker/ui/screens/configuration/ConfigurationScreen.kt
@@ -1,5 +1,6 @@
 package seamuslowry.paintracker.ui.screens.configuration
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -7,11 +8,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.ArrowLeft
+import androidx.compose.material.icons.filled.ArrowRight
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,11 +25,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.launch
 import seamuslowry.paintracker.R
 import seamuslowry.paintracker.models.ItemConfiguration
+import seamuslowry.paintracker.models.relative
 
 @Composable
 fun ConfigurationScreen(
@@ -37,7 +43,7 @@ fun ConfigurationScreen(
 
     LazyColumn(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()) {
         items(items = configurations, key = { it.id }) {
-            Text(text = it.id.toString())
+            Text(text = it.name)
         }
         item {
             if (unsavedConfiguration == null) {
@@ -70,20 +76,33 @@ fun AddConfigurationCard(
     onDiscard: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Card(modifier = modifier.fillMaxWidth()) {
-        Row(modifier = Modifier.padding(20.dp), verticalAlignment = Alignment.CenterVertically) {
+    Card(modifier = modifier.fillMaxWidth().padding(20.dp)) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(start = 20.dp, end = 10.dp, top = 10.dp, bottom = 10.dp),
+        ) {
             OutlinedTextField(
                 value = itemConfiguration.name,
                 onValueChange = { onChange(itemConfiguration.copy(name = it)) },
                 modifier = Modifier.weight(1f).padding(end = 5.dp),
                 label = { Text(text = stringResource(R.string.name)) },
             )
-            FilledIconButton(onClick = onSave) {
-                Icon(Icons.Filled.Check, contentDescription = stringResource(R.string.confirm_configuration))
+            IconButton(onClick = onDiscard) {
+                Icon(Icons.Filled.Delete, contentDescription = stringResource(R.string.discard_configuration))
             }
-            FilledIconButton(onClick = onDiscard) {
-                Icon(Icons.Filled.Close, contentDescription = stringResource(R.string.discard_configuration))
+        }
+        Row(modifier = Modifier.padding(horizontal = 5.dp), verticalAlignment = Alignment.CenterVertically) {
+            IconButton(onClick = { onChange(itemConfiguration.copy(trackingType = itemConfiguration.trackingType.relative(-1))) }) {
+                Icon(Icons.Filled.ArrowLeft, contentDescription = stringResource(R.string.change_tracking_type))
             }
+            Text(text = itemConfiguration.trackingType.name, modifier = Modifier.weight(1f), textAlign = TextAlign.Center)
+            IconButton(onClick = { onChange(itemConfiguration.copy(trackingType = itemConfiguration.trackingType.relative(1))) }) {
+                Icon(Icons.Filled.ArrowRight, contentDescription = stringResource(R.string.change_tracking_type))
+            }
+        }
+        Button(onClick = onSave, modifier = Modifier.fillMaxWidth().padding(start = 10.dp, end = 10.dp, bottom = 10.dp)) {
+            Text(text = stringResource(R.string.confirm_configuration))
         }
     }
 }
