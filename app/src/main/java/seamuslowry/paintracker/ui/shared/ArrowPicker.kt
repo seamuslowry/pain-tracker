421d451facd0abd781c3835f50cbf40c1258f3e8
Seamus Lowry <spl1owry3@gmail.com>
2023-04-15 20:39:46 -0400

    use version that doesnt mess with IDE warnings

--- app/src/main/java/seamuslowry/paintracker/ui/shared/ArrowPicker.kt
+++ app/src/main/java/seamuslowry/paintracker/ui/shared/ArrowPicker.kt
@@ -21,8 +21,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import kotlin.math.sign
 
-@Composable
 @OptIn(ExperimentalAnimationApi::class)
+@Composable
 fun ArrowPicker(
     value: Long,
     onChange: (newValue: Long) -> Unit,
