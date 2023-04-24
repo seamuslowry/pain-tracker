7194e4a1404198176e58b1fc1eb866cca2250d73
Seamus Lowry <spl1owry3@gmail.com>
2023-04-24 19:22:05 -0400

    add chips for report screen

--- app/src/main/java/seamuslowry/daytracker/ui/screens/report/ReportScreen.kt
+++ app/src/main/java/seamuslowry/daytracker/ui/screens/report/ReportScreen.kt
@@ -1,9 +1,38 @@
 package seamuslowry.daytracker.ui.screens.report
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import seamuslowry.daytracker.R
+
+enum class DisplayOption(@StringRes val label: Int) {
+    MONTH(R.string.display_month),
+    WEEK(R.string.display_week),
+}
 
 @Composable
 fun ReportScreen() {
-    Text(text = "report")
+    DisplaySelection(modifier = Modifier.fillMaxWidth())
+}
+
+@OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun DisplaySelection(
+    modifier: Modifier = Modifier,
+) {
+    FlowRow(modifier = modifier, horizontalArrangement = Arrangement.Center) {
+        DisplayOption.values().forEach {
+            FilterChip(selected = false, modifier = Modifier.padding(horizontal = 4.dp), onClick = { /*TODO*/ }, label = { Text(text = stringResource(it.label)) })
+        }
+    }
 }
