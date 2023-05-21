94071fd64235174eee1ea8b8aeced8a9ef5b8017
Seamus Lowry <spl1owry3@gmail.com>
2023-05-21 19:11:37 -0400

    add todo

--- app/src/main/java/seamuslowry/daytracker/workers/EntryReminderWorker.kt
+++ app/src/main/java/seamuslowry/daytracker/workers/EntryReminderWorker.kt
@@ -39,6 +39,13 @@ class EntryReminderWorker @AssistedInject constructor(
 
     private fun showNotification() {
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
             return
         }
 
