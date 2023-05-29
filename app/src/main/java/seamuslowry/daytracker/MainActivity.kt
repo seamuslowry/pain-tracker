41357d79d7fdcdc297803629bdb488324cdcd080
Seamus Lowry <spl1owry3@gmail.com>
2023-05-29 11:15:50 -0400

    remove testing code

--- app/src/main/java/seamuslowry/daytracker/MainActivity.kt
+++ app/src/main/java/seamuslowry/daytracker/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    Navigation(startDestination = Screen.Settings.route())
+                    Navigation(startDestination = Screen.Entry.route())
                 }
             }
         }
