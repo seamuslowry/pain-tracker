0fd4fb7aa6e0bee065e51d8f7ebe713bc99a97d3
Seamus Lowry <spl1owry3@gmail.com>
2023-06-11 08:54:32 -0400

    reset testing nav behavior

--- app/src/main/java/seamuslowry/daytracker/MainActivity.kt
+++ app/src/main/java/seamuslowry/daytracker/MainActivity.kt
@@ -23,8 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-//                    Navigation(startDestination = Screen.Entry.route(null))
-                    Navigation(startDestination = Screen.Report.route())
+                    Navigation(startDestination = Screen.Entry.route(null))
                 }
             }
         }
