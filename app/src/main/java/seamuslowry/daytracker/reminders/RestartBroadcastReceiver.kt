79ca10311bfd1e0aacaa39d93c91f3f11d94748c
Seamus Lowry <spl1owry3@gmail.com>
2024-02-18 22:41:44 -0500

    misc

--- app/src/main/java/seamuslowry/daytracker/reminders/RestartBroadcastReceiver.kt
+++ app/src/main/java/seamuslowry/daytracker/reminders/RestartBroadcastReceiver.kt
@@ -8,7 +8,7 @@ import android.util.Log
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.lastOrNull
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 import seamuslowry.daytracker.data.repos.SettingsRepo
 import javax.inject.Inject
@@ -30,15 +30,23 @@ class RestartBroadcastReceiver : BroadcastReceiver() {
 
         Log.d(TAG, "Processing restart broadcast receiver")
 
+        // TODO: maybe this shouldn't be async??
         val pending = goAsync()
 
+        // TODO: should this just be runBlocking on the settings read?
         CoroutineScope(Dispatchers.IO).launch {
             Log.d(TAG, "Entering restart broadcast receiver coroutine scope")
 
-            // if no settings, return
-            val settings = settingsRepo.settings.lastOrNull() ?: return@launch
-            // if reminders aren't enabled, return
-            if (!settings.reminderEnabled) return@launch
+            val settings = settingsRepo.settings.firstOrNull()
+
+            // if reminders aren't enabled, exit early and return
+            if (settings?.reminderEnabled != true) {
+                Log.d(TAG, "Exiting early because reminders are not enabled")
+
+                pending.finish()
+                return@launch
+            }
+
             val reminderTime = settings.reminderTime
             scheduler.scheduleReminder(reminderTime)
 
