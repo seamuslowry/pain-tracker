c5990b3e46d7d06213eb255c2e6d2f56c285940e
Seamus Lowry <spl1owry3@gmail.com>
2024-02-23 17:53:50 -0500

    chore: remove TODO

--- app/src/main/java/seamuslowry/daytracker/ui/screens/settings/SettingsViewModel.kt
+++ app/src/main/java/seamuslowry/daytracker/ui/screens/settings/SettingsViewModel.kt
@@ -17,7 +17,6 @@ class SettingsViewModel @Inject constructor(
     private val settingsRepo: SettingsRepo,
     private val scheduler: Scheduler,
 ) : ViewModel() {
-    // TODO maybe ask the scheduler as well to indicate enabled reminders????
     val state: StateFlow<Settings> = settingsRepo.settings.stateIn(
         scope = viewModelScope,
         initialValue = Settings(),
