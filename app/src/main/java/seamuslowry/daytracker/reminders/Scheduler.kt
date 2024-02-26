8acbe67ccb18c57b025f87d1fe3c9b29f158e3d4
Seamus Lowry <spl1owry3@gmail.com>
2024-02-26 14:22:09 -0500

    always use zone

--- app/src/main/java/seamuslowry/daytracker/reminders/Scheduler.kt
+++ app/src/main/java/seamuslowry/daytracker/reminders/Scheduler.kt
@@ -6,8 +6,6 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import dagger.hilt.android.qualifiers.ApplicationContext
-import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.ZonedDateTime
 import javax.inject.Inject
@@ -19,14 +17,15 @@ class Scheduler @Inject constructor(@ApplicationContext private val context: Con
         // attempt to schedule the new reminder over the old one
         val (alarmManager, pendingIntent) = alarmPieces(PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
 
-        val now = LocalDateTime.now()
-        val todayStart = LocalDateTime.of(LocalDate.now(), startTime)
-        val tomorrowStart = LocalDateTime.of(LocalDate.now().plusDays(1), startTime)
+        val now = ZonedDateTime.now()
+
+        val todayStart = now.with(startTime)
+        val tomorrowStart = todayStart.plusDays(1)
 
         val start = listOf(todayStart, tomorrowStart).firstOrNull { it.isAfter(now) } ?: todayStart
-        val epochMilli = start.toInstant(ZonedDateTime.now().offset).toEpochMilli()
+        val epochMilli = start.toInstant().toEpochMilli()
 
-        Log.d(TAG, "Scheduling reminder to run once per day at $start. Epoch Milli: $epochMilli")
+        Log.d(TAG, "Scheduling reminder to run once per day starting $start. Epoch Milli: $epochMilli")
 
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, epochMilli, AlarmManager.INTERVAL_DAY, pendingIntent!!)
     }
