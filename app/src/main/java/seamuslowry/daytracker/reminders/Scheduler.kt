97dfccbc6169c70d9328e9d6b40347d8aaec15b7
Seamus Lowry <spl1owry3@gmail.com>
2024-02-26 19:32:47 -0500

    feat/dont lose alarms (#55)
    * add action name and apply intent filter
    
    * add a restart broadcast receiver
    
    * version code
    
    * fix wrong action
    
    * spotless
    
    * misc
    
    * dont explicit cancel
    
    * just run blocking
    
    * comment
    
    * fix
    
    * spotless
    
    * chore: remove TODO
    
    * refactor: rename
    
    * chore: add timezone listener
    
    * always use zone
    
    * correct logs
    
    * correct tag

--- app/src/main/java/seamuslowry/daytracker/reminders/Scheduler.kt
+++ app/src/main/java/seamuslowry/daytracker/reminders/Scheduler.kt
@@ -6,8 +6,6 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import dagger.hilt.android.qualifiers.ApplicationContext
-import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.ZonedDateTime
 import javax.inject.Inject
@@ -16,31 +14,36 @@ private const val TAG = "Scheduler"
 
 class Scheduler @Inject constructor(@ApplicationContext private val context: Context) {
     fun scheduleReminder(startTime: LocalTime) {
-        // attempt to cancel the reminder
-        cancelReminder()
+        // attempt to schedule the new reminder over the old one
+        val (alarmManager, pendingIntent) = alarmPieces(PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
 
-        // once the old one is cancelled, attempt to schedule the new one
-        val (alarmManager, pendingIntent) = alarmPieces()
-        val now = LocalDateTime.now()
-        val todayStart = LocalDateTime.of(LocalDate.now(), startTime)
-        val tomorrowStart = LocalDateTime.of(LocalDate.now().plusDays(1), startTime)
+        val now = ZonedDateTime.now()
+
+        val todayStart = now.with(startTime)
+        val tomorrowStart = todayStart.plusDays(1)
 
         val start = listOf(todayStart, tomorrowStart).firstOrNull { it.isAfter(now) } ?: todayStart
-        val epochMilli = start.toInstant(ZonedDateTime.now().offset).toEpochMilli()
+        val epochMilli = start.toInstant().toEpochMilli()
 
-        Log.d(TAG, "Scheduling reminder to run once per day at $start. Epoch Milli: $epochMilli")
+        Log.d(TAG, "Scheduling reminder to run once per day starting $start. Epoch Milli: $epochMilli")
 
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, epochMilli, AlarmManager.INTERVAL_DAY, pendingIntent)
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, epochMilli, AlarmManager.INTERVAL_DAY, pendingIntent!!)
     }
 
-    fun cancelReminder() {
-        val (alarmManager, pendingIntent) = alarmPieces()
-        Log.d(TAG, "Cancelling reminder")
-        alarmManager.cancel(pendingIntent)
+    fun tryCancelReminder() {
+        val (alarmManager, pendingIntent) = alarmPieces(PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_NO_CREATE)
+        if (pendingIntent != null) {
+            Log.d(TAG, "Cancelling reminder")
+            alarmManager.cancel(pendingIntent)
+        }
     }
 
-    private fun alarmPieces(): Pair<AlarmManager, PendingIntent> = Pair(
+    private fun alarmPieces(flags: Int): Pair<AlarmManager, PendingIntent?> = Pair(
         context.getSystemService(Context.ALARM_SERVICE) as AlarmManager,
-        PendingIntent.getBroadcast(context, 0, Intent(context, ReminderBroadcastReceiver::class.java), PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT),
+        PendingIntent.getBroadcast(context, 0, Intent(context, ReminderBroadcastReceiver::class.java).apply { action = ACTION }, flags),
     )
+
+    companion object {
+        const val ACTION = "seamuslowry.daytracker.REMINDER_ACTION"
+    }
 }
