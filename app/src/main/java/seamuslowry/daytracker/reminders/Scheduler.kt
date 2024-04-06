3c9d6cc7d1505b94b26555597770227836be1a4f
Seamus Lowry <spl1owry3@gmail.com>
2024-04-06 16:24:42 -0400

    feat: set an alarm to reschedule reminder after timezone transitions

--- app/src/main/java/seamuslowry/daytracker/reminders/Scheduler.kt
+++ app/src/main/java/seamuslowry/daytracker/reminders/Scheduler.kt
@@ -14,8 +14,8 @@ private const val TAG = "Scheduler"
 
 class Scheduler @Inject constructor(@ApplicationContext private val context: Context) {
     fun scheduleReminder(startTime: LocalTime) {
-        // attempt to schedule the new reminder over the old one
-        val (alarmManager, pendingIntent) = alarmPieces(PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+        // attempt to schedule the new reminders over the old ones
+        val (alarmManager, reminderPendingIntent, reschedulePendingIntent) = alarmPieces(PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
 
         val now = ZonedDateTime.now()
 
@@ -23,27 +23,50 @@ class Scheduler @Inject constructor(@ApplicationContext private val context: Con
         val tomorrowStart = todayStart.plusDays(1)
 
         val start = listOf(todayStart, tomorrowStart).firstOrNull { it.isAfter(now) } ?: todayStart
+
         val epochMilli = start.toInstant().toEpochMilli()
 
         Log.d(TAG, "Scheduling reminder to run once per day starting $start. Epoch Milli: $epochMilli")
 
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, epochMilli, AlarmManager.INTERVAL_DAY, pendingIntent!!)
-    }
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, epochMilli, AlarmManager.INTERVAL_DAY, reminderPendingIntent!!)
 
-    fun tryCancelReminder() {
-        val (alarmManager, pendingIntent) = alarmPieces(PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_NO_CREATE)
-        if (pendingIntent != null) {
-            Log.d(TAG, "Cancelling reminder")
-            alarmManager.cancel(pendingIntent)
+        Log.d(TAG, "Attempting to schedule next transition reschedule.")
+        val nextTransition = start.zone.rules.nextTransition(start.toInstant())
+        Log.d(TAG, "Next transition in ${start.zone} is ${nextTransition}.")
+
+        if (nextTransition != null) {
+            val rescheduleDate = nextTransition.dateTimeAfter.atZone(nextTransition.offsetAfter)
+            val rescheduleMillis = rescheduleDate.toInstant().toEpochMilli()
+            Log.d(TAG, "Scheduling transition reschedule to run once $rescheduleDate. Epoch Milli: $rescheduleMillis")
+
+            alarmManager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, rescheduleMillis, reschedulePendingIntent!!)
+        } else {
+            Log.d(TAG, "Cancelling transition reschedule since timezone has no transitions")
+
+            alarmManager.cancel(reschedulePendingIntent!!)
         }
     }
 
-    private fun alarmPieces(flags: Int): Pair<AlarmManager, PendingIntent?> = Pair(
+    fun tryCancelReminder() {
+        val (alarmManager, reminderPendingIntent, reschedulePendingIntent) = alarmPieces(PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_NO_CREATE)
+        if (reminderPendingIntent != null) {
+            Log.d(TAG, "Cancelling reminder")
+            alarmManager.cancel(reminderPendingIntent)
+        }
+        if (reschedulePendingIntent != null) {
+            Log.d(TAG, "Cancelling transition reschedule")
+            alarmManager.cancel(reschedulePendingIntent)
+        }
+    }
+
+    private fun alarmPieces(flags: Int): Triple<AlarmManager, PendingIntent?, PendingIntent?> = Triple(
         context.getSystemService(Context.ALARM_SERVICE) as AlarmManager,
-        PendingIntent.getBroadcast(context, 0, Intent(context, ReminderBroadcastReceiver::class.java).apply { action = ACTION }, flags),
+        PendingIntent.getBroadcast(context, 0, Intent(context, ReminderBroadcastReceiver::class.java).apply { action = REMINDER_ACTION }, flags),
+        PendingIntent.getBroadcast(context, 0, Intent(context, RescheduleReminderBroadcastReceiver::class.java).apply { action = ZONE_TRANSITION_RESCHEDULE_ACTION }, flags),
     )
 
     companion object {
-        const val ACTION = "seamuslowry.daytracker.REMINDER_ACTION"
+        const val REMINDER_ACTION = "seamuslowry.daytracker.REMINDER_ACTION"
+        const val ZONE_TRANSITION_RESCHEDULE_ACTION = "seamuslowry.daytracker.ZONE_TRANSITION_RESCHEDULE_ACTION"
     }
 }
