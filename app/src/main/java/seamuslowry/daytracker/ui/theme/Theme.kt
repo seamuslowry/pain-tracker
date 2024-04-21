7f5d05c8a8178072d56d0511eae8a4628a35a96b
Seamus Lowry <spl1owry3@gmail.com>
2024-04-21 09:11:23 -0400

    deps/update (#60)
    * chore: versions
    
    * deps: update dependencies
    
    * chore: add todo
    
    * fix: spotless handle composable

--- app/src/main/java/seamuslowry/daytracker/ui/theme/Theme.kt
+++ app/src/main/java/seamuslowry/daytracker/ui/theme/Theme.kt
@@ -1,7 +1,6 @@
 package seamuslowry.daytracker.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -34,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
-    */
+     */
 )
 
 @Composable
@@ -45,7 +44,7 @@ fun DayTrackerTheme(
     content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
