262c52321a546cec3720600900ca5d17030b0def
Seamus Lowry <spl1owry3@gmail.com>
2024-07-15 16:53:36 -0400

    preferred upsert implementation

--- app/src/main/java/seamuslowry/daytracker/data/room/daos/ItemConfigurationDao.kt
+++ app/src/main/java/seamuslowry/daytracker/data/room/daos/ItemConfigurationDao.kt
@@ -3,7 +3,6 @@ package seamuslowry.daytracker.data.room.daos
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
@@ -14,7 +13,7 @@ interface ItemConfigurationDao {
     @Query("select * from item_configuration")
     fun getAll(): Flow<List<ItemConfiguration>>
 
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    @Insert
     suspend fun insert(itemConfiguration: ItemConfiguration): Long
 
     @Update
@@ -22,12 +21,11 @@ interface ItemConfigurationDao {
 
     // need a custom upsert because OnConflictStrategy.REPLACE does a delete and insert
     suspend fun upsert(itemConfiguration: ItemConfiguration): Long {
-        val result = insert(itemConfiguration)
-        if (result < 0) {
+        if (itemConfiguration.id > 0) {
             update(itemConfiguration)
             return itemConfiguration.id
         }
-        return result
+        return insert(itemConfiguration)
     }
 
     @Delete
