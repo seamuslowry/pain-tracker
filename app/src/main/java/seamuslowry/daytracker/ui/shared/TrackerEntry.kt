a78fb25aa3416270dc1316e95d3992807d94f842
Seamus Lowry <spl1owry3@gmail.com>
2024-08-29 06:53:25 -0400

    feat/material segmented button (#47)
    * feat: try material segmented button
    
    * docs: add comment on why it wont work for now
    
    * chore: versions
    
    * chore: spotless
    
    * iterate
    
    * versions and remove dead code
    
    * feat: make package segmented buttons work

--- app/src/main/java/seamuslowry/daytracker/ui/shared/TrackerEntry.kt
+++ app/src/main/java/seamuslowry/daytracker/ui/shared/TrackerEntry.kt
@@ -1,18 +1,28 @@
 package seamuslowry.daytracker.ui.shared
 
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import seamuslowry.daytracker.R
 import seamuslowry.daytracker.models.Item
 import seamuslowry.daytracker.models.LimitedOptionTrackingType
 import seamuslowry.daytracker.models.TextEntryTrackingType
 import seamuslowry.daytracker.models.TrackingType
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TrackerEntry(
     trackerType: TrackingType,
@@ -22,16 +32,50 @@ fun TrackerEntry(
     enabled: Boolean = true,
 ) {
     when (trackerType) {
-        is LimitedOptionTrackingType -> SegmentedButtons(
-            values = trackerType.options,
-            value = trackerType.options.find { it.value == item?.value },
-            onChange = { option -> item?.let { onChange(it.copy(value = option.value)) } },
-            enabled = enabled,
-            modifier = modifier
-                .defaultMinSize(minHeight = OutlinedTextFieldDefaults.MinHeight),
-        ) {
-            Text(text = it.text?.let { text -> stringResource(id = text) } ?: it.value.toString())
-        }
+        is LimitedOptionTrackingType ->
+            SingleChoiceSegmentedButtonRow(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .defaultMinSize(minHeight = OutlinedTextFieldDefaults.MinHeight),
+            ) {
+                trackerType.options.forEachIndexed { index, option ->
+                    SegmentedButton(
+                        enabled = enabled,
+                        selected = option.value == item?.value,
+                        icon = {},
+                        onClick = { item?.let { onChange(it.copy(value = option.value)) } },
+                        shape = SegmentedButtonDefaults.itemShape(
+                            index = index,
+                            count = trackerType.options.size,
+                        ),
+                        colors = SegmentedButtonDefaults.colors(
+                            // active buttons use the default button colors
+                            activeContainerColor = ButtonDefaults.buttonColors().containerColor,
+                            activeContentColor = ButtonDefaults.buttonColors().contentColor,
+                            disabledActiveContainerColor = ButtonDefaults.buttonColors().disabledContainerColor,
+                            disabledActiveContentColor = ButtonDefaults.buttonColors().disabledContentColor,
+                            // inactive buttons use the default outlined button colors
+                            inactiveContainerColor = ButtonDefaults.outlinedButtonColors().containerColor,
+                            inactiveContentColor = ButtonDefaults.outlinedButtonColors().contentColor,
+                            disabledInactiveContainerColor = ButtonDefaults.outlinedButtonColors().disabledContainerColor,
+                            disabledInactiveContentColor = ButtonDefaults.outlinedButtonColors().disabledContentColor,
+                            // all borders use outline
+                            activeBorderColor = MaterialTheme.colorScheme.outline,
+                            inactiveBorderColor = MaterialTheme.colorScheme.outline,
+                            disabledActiveBorderColor = MaterialTheme.colorScheme.outline,
+                            disabledInactiveBorderColor = MaterialTheme.colorScheme.outline,
+                        ),
+                    ) {
+                        Text(
+                            text = option.text?.let { text -> stringResource(id = text) }
+                                ?: option.value.toString(),
+                            softWrap = false,
+                            modifier = Modifier.requiredWidth(IntrinsicSize.Max),
+                            overflow = TextOverflow.Visible,
+                        )
+                    }
+                }
+            }
         is TextEntryTrackingType -> DelayedSaveTextField(
             onSave = { newText -> item?.let { onChange(it.copy(comment = newText, value = -1)) } },
             value = item?.comment ?: "",
