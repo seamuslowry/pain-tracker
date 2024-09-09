cda4d5864634efa872d6b80f826082c9265adf56
Seamus Lowry <spl1owry3@gmail.com>
2024-09-09 12:59:50 -0400

    remove unused

--- app/src/main/java/seamuslowry/daytracker/ui/screens/entry/EntryScreen.kt
+++ app/src/main/java/seamuslowry/daytracker/ui/screens/entry/EntryScreen.kt
@@ -23,7 +23,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
